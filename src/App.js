c601df4ce6a7638abfedc950ab8abdf8838e51d7
Milu Franz <mfranz@Milus-MacBook-Pro.local>
2019-09-07 19:13:40 -0400

    Add labels and title

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
   const [activeCals, setActiveCals] = useState([]);
   const [restingCals, setRestingCals] = useState([]);
   const [total, setTotal] = useState([]);
+  const [stdAvg, setStdAvg] = useState([]);
+  const [myTotalAvg, setMyTotalAvg] = useState([]);
 
   useEffect(() => {
     store.read("sheet1", { limit: 64, offset: 1 }).then(data => {
@@ -22,29 +24,56 @@ function App() {
 
   useEffect(() => {
     if (data && data.length > 0) {
+      const STD_AVG = 1292;
+
       let dayArray = [];
       let activeCalsArray = [];
       let restingCalsArray = [];
       let totalArray = [];
+      let stdAvgArray = [];
+      let myTotalAvgArray = [];
+      let totalSum = 0;
 
       data.forEach(item => {
         dayArray.push(item.day);
         activeCalsArray.push(item.active_calories);
         restingCalsArray.push(item.resting_calories);
         let sum = Number(item.active_calories) + Number(item.resting_calories);
+        totalSum += sum;
         totalArray.push(sum);
+        stdAvgArray.push(STD_AVG);
+        myTotalAvgArray.push(0);
       });
+
+      let totalAvgCalc = totalSum / data.length;
+      myTotalAvgArray.fill(totalAvgCalc, 0, data.length);
 
       setDays(dayArray);
       setActiveCals(activeCalsArray);
       setRestingCals(restingCalsArray);
       setTotal(totalArray);
+      setStdAvg(stdAvgArray);
+      setMyTotalAvg(myTotalAvgArray);
     }
   }, [data]);
 
   const stackedGraphData = {
     labels: days,
     datasets: [
+      {
+        label: "Milu's Average Active Metabolic Rate",
+        borderColor: "pink",
+        backgroundColor: "transparent",
+        stack: "3",
+        data: myTotalAvg
+      },
+      {
+        label: "Standard Average Resting Metabolic Rate",
+        borderColor: "yellow",
+        backgroundColor: "transparent",
+        stack: "3",
+        data: stdAvg
+      },
       {
         label: "Active Calories",
         backgroundColor: "#63aabc",
@@ -66,29 +95,34 @@ function App() {
     ]
   };
 
-  const barChartOptions = {
-    legend: {
-      display: false
-    },
-    scales: {
-      xAxes: [
-        {
-          stacked: true
-        }
-      ],
-      yAxes: [
-        {
-          stacked: true
-        }
-      ]
-    }
-  };
-
-  console.log({ total });
-
   return (
     <div className="App">
-      <Line data={stackedGraphData} />
+      <Line
+        data={stackedGraphData}
+        width={70}
+        height={400}
+        options={{
+          maintainAspectRatio: false,
+          legend: {
+            position: "bottom"
+          },
+          scales: {
+            yAxes: [
+              {
+                scaleLabel: {
+                  display: true,
+                  labelString: "Calories"
+                }
+              }
+            ]
+          },
+          title: {
+            display: true,
+            text: "Milu's Caloric Expenditure",
+            fontSize: 20
+          }
+        }}
+      />
     </div>
   );
 }
